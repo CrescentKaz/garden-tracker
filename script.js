5de12550bf6383ab72dc4eea0cc2df6c108ed5fd
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-06-27 12:05:29 -0500

    created divs for plot 1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // js file
+
+const planted = [
+    {
+        location: p1c13,
+        item: "Chocolate Mint",
+        planted: "",
+        harvest: "AN"
+    },
+    {
+        location: p1c12,
+        item: "Chotolate Mint",
+        planted: "",
+        harvest: "AN"
+    }
+];
