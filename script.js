fe4500a5923a74646e007ae0ffd4d36c83c4caea
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-07-02 12:06:45 -0500

    added css changes to the maps and basic functionality to the buttons

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// js file
-
 const planted = [
     {
         location: p1c13,
@@ -14,3 +12,41 @@ const planted = [
         harvest: "AN"
     }
 ];
+
+function showHome() {
+    homeSection.style.display = "contents";
+    plot1Section.style.display = "none";
+    plot2Section.style.display = "none";
+    notesSection.style.display = "none";
+}
+
+function showPlot1() {
+    homeSection.style.display = "none";
+    plot1Section.style.display = "contents";
+    plot2Section.style.display = "none";
+    notesSection.style.display = "none";
+}
+
+function showPlot2() {
+    homeSection.style.display = "none";
+    plot1Section.style.display = "none";
+    plot2Section.style.display = "contents";
+    notesSection.style.display = "none";
+}
+
+function showNotes() {
+    homeSection.style.display = "none";
+    plot1Section.style.display = "none";
+    plot2Section.style.display = "none";
+    notesSection.style.display = "contents";
+//    displayNotes();
+}
+
+/*
+function displayNotes() {
+    for () {
+        gardenNotes.innerText += planted[i].item + " was planted on " + planted[i].planted + " in " + planted[i].location;
+        gardenNotes.innerText += "Estimated harvest is " + planted[i].harvest;
+    }
+}
+*/
