22d2c073d838d749963233a18d911497c1ebc322
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-09-04 12:12:20 -0500

    added plot logs for plot 1 and 2, added location names for the rest of plot 1, added a dummy entry for plot 2 planted (for testing purposes), changed the spot and cell size to scale based on the width of the viewport, and attempted to center (horizontally) the maps for both plots.

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const gardenNotes = document.querySelector("#gardenNotes");
-//const p1c1 = document.getElementById(p1c1);
-//const p1c12 = document.getElementById(p1c12);
-//const p1c13 = document.getElementById(p1c13);
+const plot1Log = document.getElementById("plot1Log");
+const plot2Log = document.getElementById("plot2Log");
 
 const planted = [
     {
         plot: 1,
         location: "C13",
         item: "Chocolate Mint",
-        planted: "unknown",
-        harvest: "AN"
+        planted: "unknown date",
+        harvest: "as needed"
     },
     {
         plot: 1,
         location: "C12",
         item: "Chotolate Mint",
-        planted: "unknown",
-        harvest: "AN"
+        planted: "unknown date",
+        harvest: "as needed"
+    },
+    {
+        plot: 2,
+        location: "S23?",
+        item: "corn",
+        planted: "unknown date",
+        harvest: "NA... ants killed the whole plant"
     }
 ];
 
@@ -51,12 +57,40 @@ function showNotes() {
     displayNotes();
 }
 
+function filterByP1(item) {
+    if (item.plot = 1) {
+        return true;
+    };
+    return false;
+}
+
+function filterByP2(item) {
+    if (item.plot = 2) {
+        return true;
+    };
+    return false;
+}
+
 function displayPlot1Notes() {
     console.log("plot 1 notes");
+    let p1Len = planted.filter(filterByP1);
+    let text = ""; 
+    for (let i = 0; i < p1Len; i++) {
+        text += p1Len[i].item + " was planted on " + p1Len[i].planted + " in " + p1Len[i].location + 
+        ". \n Estimated harvest is " + p1Len[i].harvest + ". \n \n";
+    };
+    plot1Log.innerText = text;
 }
 
 function displayPlot2Notes() {
     console.log("plot 2 notes");
+    let p2Len = planted.filter(filterByP2);
+    let text = ""; 
+    for (let i = 0; i < p2Len; i++) {
+        text += p2Len[i].item + " was planted on " + p2Len[i].planted + " in " + p2Len[i].location + 
+        ". \n Estimated harvest is " + p2Len[i].harvest + ". \n \n";
+    };
+    plot2Log.innerText = text;
 }
 
 function displayNotes() {
@@ -65,7 +99,7 @@ function displayNotes() {
     let text = "";
     for (let i = 0; i < len; i++) {
         text += planted[i].item + " was planted on " + planted[i].planted + " in Plot " + planted[i].plot + ", " + planted[i].location + 
-        "<br>Estimated harvest is " + planted[i].harvest + "<br><br>";
+        ". \n Estimated harvest is " + planted[i].harvest + ". \n \n";
     };
     gardenNotes.innerText = text;
 }
