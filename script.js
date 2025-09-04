a6ef4fd3aadf02bee5883dba7349986de6a01e97
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2025-09-04 11:16:46 -0500

    added quotation marks to the querySelector

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gardenNotes = document.querySelector(#gardenNotes);
+const gardenNotes = document.querySelector("#gardenNotes");
 //const p1c1 = document.getElementById(p1c1);
 //const p1c12 = document.getElementById(p1c12);
 //const p1c13 = document.getElementById(p1c13);
@@ -64,7 +64,8 @@ function displayNotes() {
     let len = planted.length;
     let text = "";
     for (let i = 0; i < len; i++) {
-        text += planted[i].item + " was planted on " + planted[i].planted + " in Plot " + planted[i].plot + ", " + planted[i].location + "<br>Estimated harvest is " + planted[i].harvest + "<br><br>";
+        text += planted[i].item + " was planted on " + planted[i].planted + " in Plot " + planted[i].plot + ", " + planted[i].location + 
+        "<br>Estimated harvest is " + planted[i].harvest + "<br><br>";
     };
     gardenNotes.innerText = text;
 }
